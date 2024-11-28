7caa62a972fc7461bcce240055626bd323a52e1c
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-28 15:43:24 +0000

    calender

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed } from '@fortawesome/free-solid-svg-icons'
+import { faBed, faCalendarDays, faPerson } from '@fortawesome/free-solid-svg-icons'
 import {faPlane} from '@fortawesome/free-solid-svg-icons'
 import {faCar} from '@fortawesome/free-solid-svg-icons'
 import {faTaxi} from '@fortawesome/free-solid-svg-icons'
-
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import {format} from 'date-fns';
 const Header = () => {
+  const [date, setDate] = useState([
+    {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection'
+    }
+  ]);
   return (
     <div className='header'>
       <div className="headerContainer">
@@ -39,9 +49,33 @@ const Header = () => {
           with a free Hamed miri app account
         </p>
         <button className='headerBtn'>Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+          <FontAwesomeIcon icon={faBed} className='headerIcon'/>
+            <input type="text" placeholder='Where are you going' className='headerSearchInput' />
+          </div>
+          <div className="headerSearchItem">
+          <FontAwesomeIcon icon={faCalendarDays} className='headerIcon'/>
+          <span className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} 
+              to ${format(date[0].endDate, "MM/dd/yyyy")}`}
+          </span>
+
+              <DateRange
+                editableDateInputs={true}
+                onChange={item => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date} className='date'/>
+          </div>
+          <div className="headerSearchItem">
+          <FontAwesomeIcon icon={faPerson} className='headerIcon'/>
+           <span className='headerSearchText'>2 adults 2 childeren 1 room</span>
+          </div>
+          <div className="headerSearchItem">
+          <button className='headerBtn'>Search</button>
+          </div>
+        </div>
         </div>
     </div>
   )
 }
-
-export default Header
+export default Header;
