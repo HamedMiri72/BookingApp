586a8f29b17ad860026befde6fac81025dc54852
HamedMiri72 <hamedmiri72@gmail.com>
2024-11-28 21:28:05 +0000

    click calender

--- booking/src/components/header/Header.jsx
+++ booking/src/components/header/Header.jsx
@@ -10,6 +10,7 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import {format} from 'date-fns';
 const Header = () => {
+  const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
     {
     startDate: new Date(),
@@ -56,15 +57,15 @@ const Header = () => {
           </div>
           <div className="headerSearchItem">
           <FontAwesomeIcon icon={faCalendarDays} className='headerIcon'/>
-          <span className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} 
+          <span onClick={() => setOpenDate(!openDate)} className='headerSearchText'>{`${format(date[0].startDate, "MM/dd/yyyy")} 
               to ${format(date[0].endDate, "MM/dd/yyyy")}`}
           </span>
 
-              <DateRange
+              {openDate && <DateRange
                 editableDateInputs={true}
                 onChange={item => setDate([item.selection])}
                 moveRangeOnFirstSelection={false}
-                ranges={date} className='date'/>
+                ranges={date} className='date'/>}
           </div>
           <div className="headerSearchItem">
           <FontAwesomeIcon icon={faPerson} className='headerIcon'/>
