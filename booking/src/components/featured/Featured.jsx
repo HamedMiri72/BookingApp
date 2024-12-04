d3374aba239df96cbc01d455d6fc569c7b7e0fa6
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-04 21:24:32 +0000

    Featured

---
+++ booking/src/components/featured/Featured.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className="featuredItem">
+            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Edinbrugh</h1>
+                <h2>123 Propertices</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img className='FeaturedImg'  src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Edinbrugh</h1>
+                <h2>123 Propertices</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Edinbrugh</h1>
+                <h2>123 Propertices</h2>
+            </div>
+        </div>
+        <div className="featuredItem">
+            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+            <div className="featuredTitles">
+                <h1>Edinbrugh</h1>
+                <h2>123 Propertices</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Home.css"
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
+import Featured from '../../components/featured/Featured'
 
 
 
@@ -10,6 +11,9 @@ const Home = () => {
     <div>
       <Navbar/>
       <Header/>
+      <div className='homeContainer'>
+        <Featured/>
+      </div>
     </div>
   )
 }
