fcaa2234de61fa0263b45816e6b69ae5af8fdf0f
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-04 23:15:51 +0000

    property type

---
+++ booking/src/components/PropertyList/PropertyList.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import "./PropertyList.css"
+
+const PropertyList = () => {
+  return (
+    <div className='pList'>
+        <div className="PlistItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370297935.jpg?k=a1704ba2443d4210f076beb77dd017c54bac9e1e3f2cf1c391864bfbb2e7edc9&o=&hp=1" alt="" className='pListImg' />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+
+        <div className="PlistItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370297935.jpg?k=a1704ba2443d4210f076beb77dd017c54bac9e1e3f2cf1c391864bfbb2e7edc9&o=&hp=1" alt="" className='pListImg' />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+
+        <div className="PlistItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370297935.jpg?k=a1704ba2443d4210f076beb77dd017c54bac9e1e3f2cf1c391864bfbb2e7edc9&o=&hp=1" alt="" className='pListImg' />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+
+        <div className="PlistItem">
+            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370297935.jpg?k=a1704ba2443d4210f076beb77dd017c54bac9e1e3f2cf1c391864bfbb2e7edc9&o=&hp=1" alt="" className='pListImg' />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>233 hotels</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PropertyList
--- booking/src/pages/home/Home.jsx
+++ booking/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css"
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/PropertyList/PropertyList'
 
 
 
@@ -14,6 +15,7 @@ const Home = () => {
       <div className='homeContainer'>
         <Featured/>
         <h1 className='homeTitle'>Browse by property type</h1>
+        <PropertyList/>
       </div>
     </div>
   )
