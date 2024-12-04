427d98f088be622c38709832a438b9fdf2f553ae
HamedMiri72 <hamedmiri72@gmail.com>
2024-12-04 21:47:28 +0000

    featured Finished

--- booking/src/components/featured/Featured.jsx
+++ booking/src/components/featured/Featured.jsx
@@ -5,28 +5,21 @@ const Featured = () => {
   return (
     <div className='featured'>
         <div className="featuredItem">
-            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=&hp=1" alt="" className='FeaturedImg' />
             <div className="featuredTitles">
                 <h1>Edinbrugh</h1>
                 <h2>123 Propertices</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img className='FeaturedImg'  src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=&hp=1" alt="" className='FeaturedImg' />
             <div className="featuredTitles">
                 <h1>Edinbrugh</h1>
                 <h2>123 Propertices</h2>
             </div>
         </div>
         <div className="featuredItem">
-            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
-            <div className="featuredTitles">
-                <h1>Edinbrugh</h1>
-                <h2>123 Propertices</h2>
-            </div>
-        </div>
-        <div className="featuredItem">
-            <img className='FeaturedImg' src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=" alt="" />
+        <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/370314710.jpg?k=ea5d8d79945f3c79afd15cecf0e71cc0f0f293358ece11d3e348ccb803d56f6d&o=&hp=1" alt="" className='FeaturedImg' />
             <div className="featuredTitles">
                 <h1>Edinbrugh</h1>
                 <h2>123 Propertices</h2>
